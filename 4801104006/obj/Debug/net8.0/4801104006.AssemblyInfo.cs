95ac33f2e46d6fc3019301bc82fdf55922b78fd1
anhleh33 <166407355+anhleh33@users.noreply.github.com>
2025-04-01 07:07:57 +0700

    New project to push to Github.

--- 4801104006/obj/Debug/net8.0/4801104006.AssemblyInfo.cs
+++ 4801104006/obj/Debug/net8.0/4801104006.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("4801104006")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8e3182027065f8dc9fe6b76dcbfc55b56506787")]
 [assembly: System.Reflection.AssemblyProductAttribute("4801104006")]
 [assembly: System.Reflection.AssemblyTitleAttribute("4801104006")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- TestProject1/obj/Debug/net8.0/TestProject1.AssemblyInfo.cs
+++ TestProject1/obj/Debug/net8.0/TestProject1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8e3182027065f8dc9fe6b76dcbfc55b56506787")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
